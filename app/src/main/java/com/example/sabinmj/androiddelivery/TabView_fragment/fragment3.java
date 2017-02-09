d4ea673c2c616f76543d145c47278cca17c886fa
sabin <sabin.mj@gmail.com>
2017-02-09 10:11:18 +0530

    UI update
    first update

---
+++ app/src/main/java/com/example/sabinmj/androiddelivery/TabView_fragment/fragment3.java
@@ -0,0 +1,21 @@
+package com.example.sabinmj.androiddelivery.TabView_fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.sabinmj.androiddelivery.R;
+
+/**
+ * Created by Sabin M J on 08-Feb-17.
+ */
+public class fragment3 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_three, container, false);
+    }
+}
